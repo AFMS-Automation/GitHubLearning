904aa8870e1eea7c3428838117f4e6f19fbcb031
AFMS-Automation <ravitejsahu19@gmail.com>
2024-05-29 23:39:33 +0530

    Adding to cart branch

--- src/Tests/CartPageTest.java
+++ src/Tests/CartPageTest.java
@@ -1,12 +1,10 @@
 package Tests;
 
-public class HomePageTest {
+public class CartPageTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("New Home page test is added");
-		
 	}
 
 }
--- src/Tests/LogoutPageTest.java
+++ src/Tests/LogoutPageTest.java
@@ -4,5 +4,8 @@ public class LogoutPageTest {
 	
 	/*Test logout page is added*/
 	String str = "Ravitej";
+	String flat = "D417";
+	String block ="d";
+	
 
 }
