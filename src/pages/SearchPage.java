6e2b2d58eaea970337350a3c4e7b743e938f19cb
AFMS-Automation <ravitejsahu19@gmail.com>
2024-05-29 11:42:33 +0530

    Added new print line in search page

--- src/pages/SearchPage.java
+++ src/pages/SearchPage.java
@@ -4,7 +4,7 @@ public class SearchPage {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		System.out.println("New code for search");
 		/*I use this page to search*/
 
 	}
