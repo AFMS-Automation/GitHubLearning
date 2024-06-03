f92ab831170e2d12ca6a57a096945c8f0203e4fe
AFMS-Automation <ravitejsahu19@gmail.com>
2024-06-03 18:57:38 +0530

    FEATURE ONE

--- src/pages/Feature.java
+++ src/pages/Feature.java
@@ -5,7 +5,7 @@ public class Feature {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Fetaure one");
+		System.out.println("Fetaure 1");
 
 	}
 
