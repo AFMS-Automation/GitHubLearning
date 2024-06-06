74c867f2177ce92f48dec5aa27aa2e928fd2dd01
AFMS-Automation <ravitejsahu19@gmail.com>
2024-06-06 22:47:14 +0530

    New pages payment is added

--- src/pages/AddPayment.java
+++ src/pages/AddPayment.java
@@ -1,13 +1,11 @@
 package pages;
 
-public class Feature {
+public class AddPayment {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Fetaure 1");
-		
-		System.out.println("Training Day 2");
+		System.out.println("Paymaent page added");
 
 	}
 
