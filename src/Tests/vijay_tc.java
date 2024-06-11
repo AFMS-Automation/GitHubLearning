3fd1ac55fe96af14d4194636c48b4de35ad07932
vijay511998 <vijaykumarsahu058@gmail.com>
2024-06-11 23:05:38 +0530

    vijay script added n june 11

---
+++ src/Tests/vijay_tc.java
@@ -0,0 +1,11 @@
+package Tests;
+
+public class vijay_tc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Vijay's code on june 11");
+
+	}
+
+}
---
+++ src/Tests/vijay_tc2.java
@@ -0,0 +1,10 @@
+package Tests;
+
+public class vijay_tc2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("Vijay Code change 1");
+	}
+
+}
